554b9f7e4c212717a99fed94436a7d95381109bc
georgebrown <cyrus_b_uk@server>
2020-01-23 21:46:33 +0100

    Initial Commit

---
+++ syphon-kinect-metal-client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  syphon-kinect-metal-client
+//
+//  Created by George Brown on 23.01.20.
+//  Copyright © 2020 George Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ syphon-kinect-metal-clientTests/syphon_kinect_metal_clientTests.swift
@@ -0,0 +1,34 @@
+//
+//  syphon_kinect_metal_clientTests.swift
+//  syphon-kinect-metal-clientTests
+//
+//  Created by George Brown on 23.01.20.
+//  Copyright © 2020 George Brown. All rights reserved.
+//
+
+import XCTest
+@testable import syphon_kinect_metal_client
+
+class syphon_kinect_metal_clientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
